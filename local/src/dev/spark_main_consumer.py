afe87077af20d22a36f707908939536d2bc2debe
GonzaloAlcalaGD <galcala@griddynamics.com>
2023-03-22 14:55:59 -0600

    refactor topic consuming logic

--- local/src/dev/spark_main_consumer.py
+++ local/src/dev/spark_main_consumer.py
@@ -1,36 +1,9 @@
 import logging
+import importlib
 from pyspark.sql import SparkSession
-from pyspark.sql.functions import from_json, col, lit
-from spark_utils import incoming_schemas as schemas
-from spark_utils import casting_strings as cast
+
 
 logging.basicConfig(level=logging.INFO)
-
-schema_dict: dict = {
-    "rtppmdata.nationalpage.nationalppm": schemas.nationalpage_national_ppm,
-    "rtppmdata.nationalpage.sector": schemas.nationalpage_national_sector,
-    "rtppmdata.nationalpage.operator": schemas.nationalpage_national_operator,
-    "rtppmdata.oocpage.operator": schemas.oocpage_operator,
-    "rtppmdata.focpage.nationalppm": schemas.focpage_nationalppm,
-    "rtppmdata.focpage.operator": schemas.focpage_operator,
-    "rtppmdata.operatorpage.operators": schemas.operatorpage_operators,
-    "rtppmdata.operatorpage.servicegroups": schemas.operatorpage_service_operators,
-}
-
-casting_dict: dict = {
-    "rtppmdata.nationalpage.nationalppm": cast.nationalpage_nationalppm,
-    "rtppmdata.nationalpage.sector": None,
-    "rtppmdata.nationalpage.operator": None,
-    "rtppmdata.oocpage.operator": None,
-    "rtppmdata.focpage.nationalppm": None,
-    "rtppmdata.focpage.operator": None,
-    "rtppmdata.operatorpage.operators": None,
-    "rtppmdata.operatorpage.servicegroups": None,
-}
-
-
-def proccessData(topic: str, df) -> None:
-    return logging.info(f"Processing df from topic: {topic}")
 
 
 class SparkConsumer:
@@ -51,7 +24,8 @@ class SparkConsumer:
         self.kafka_topics = kafka_topics
 
     def consumeTopics(self):
-        """"""
+        """ """
+
         packages = [
             f"org.apache.spark:spark-sql-kafka-0-10_{self.scala_version}:{self.spark_version}",
             f"org.apache.kafka:kafka-clients:{self.kafka_client}",
@@ -64,37 +38,20 @@ class SparkConsumer:
             .config("spark.executor.cores", "8")
             .config("spark.executor.memory", "8g")
             .config("spark.jars.packages", ",".join(packages))
-            .config("spark.sql.shuffle.partitions", 8)
+            .config("spark.sql.shuffle.partitions", 16)
             .getOrCreate()
         )
 
         for topic in self.kafka_topics:
-            casting_expr = casting_dict.get(topic)
-            kafka_options = {
-                "kafka.bootstrap.servers": f"{self.kafka_host}:{self.kafka_port}",
-                "subscribe": topic,
-                "startingOffsets": "latest",
-            }
 
-            df = (
-                spark.readStream.format("kafka")
-                .options(**kafka_options)
-                .load()
-                .selectExpr("CAST(value AS STRING)")
-                .select(from_json(col("value"), schema_dict.get(topic)).alias("data"))
-                .select("data.*")
-                .selectExpr(casting_expr)
+            topic_name = topic.replace(".", "_")
+
+            process_module = importlib.import_module(
+                name=f"spark_utils.process_topic_{topic_name}"
             )
+            process_func = process_module.process_topic
 
-            df.printSchema()
-            df = df.dropDuplicates()
-
-            query = (
-                df.writeStream.outputMode("update")
-                .format("console")
-                .option("truncate", "false")
-                .start()
-            )
+            query = process_func(spark, topic, self.kafka_host, self.kafka_port)
 
             queries.append(query)
 
@@ -111,13 +68,13 @@ if __name__ == "__main__":
     kafka_port: str = "9092"
     kafka_topics: list = [
         "rtppmdata.nationalpage.nationalppm",
-        # "rtppmdata.nationalpage.sector",
-        # "rtppmdata.nationalpage.operator",
-        # "rtppmdata.oocpage.operator",
-        # "rtppmdata.focpage.nationalppm",
-        # "rtppmdata.focpage.operator",
-        # "rtppmdata.operatorpage.operators",
-        # "rtppmdata.operatorpage.servicegroups",
+        "rtppmdata.nationalpage.sector",
+        "rtppmdata.nationalpage.operator",
+        "rtppmdata.oocpage.operator",
+        "rtppmdata.focpage.nationalppm",
+        "rtppmdata.focpage.operator",
+        "rtppmdata.operatorpage.operators",
+        "rtppmdata.operatorpage.servicegroups",
     ]
 
     consumer = SparkConsumer(
